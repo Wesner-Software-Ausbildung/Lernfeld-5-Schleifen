b12a528dbd9d1782458cf73c5b78fa98df14b1bc
Wesner-Software-Ausbildung <sergei.pron@wesner-software.de>
2023-10-17 22:08:29 +0200

    Eine Teil von diese Aufgabe habe ich gemacht. Ich muss noch zweite Teil bis Ende machen. Es wird nicht so schwer, weil zweite Teil ein bißchen schwierig ist.

--- src/numberListGenerator/Generator.java
+++ src/numberListGenerator/Generator.java
@@ -3,10 +3,23 @@ package numberListGenerator;
 public class Generator {
 
     public String generateNumbers(int start, int end) {
-        return "";
+
+        if (start >= end) {
+            return "";
+        }
+        String result = "";
+
+        for (int i = start; i <= end; i++) {
+            result += i + ",";
+        }
+
+        result = result.substring(0, result.length() -1);
+        return result;
     }
 
     public String generateNumbers(int start, int end, int steps, char delimiter) {
+
+
         return "";
     }
 
